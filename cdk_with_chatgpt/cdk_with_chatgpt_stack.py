800219b19868f1fd93d22d7c2595181856d4914e
MAEKAWA Hiroshi <hiroshi1.maekawa@daikin.co.jp>
2023-04-05 08:39:38 +0900

    Add S3 bucket

--- cdk_with_chatgpt/cdk_with_chatgpt_stack.py
+++ cdk_with_chatgpt/cdk_with_chatgpt_stack.py
@@ -1,8 +1,4 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+from aws_cdk import RemovalPolicy, Stack, aws_s3 as s3
 from constructs import Construct
 
 class CdkWithChatgptStack(Stack):
@@ -10,10 +6,7 @@ class CdkWithChatgptStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Create an Amazon S3 bucket
+        s3.Bucket(self, "MyBucket", versioned=True, removal_policy=RemovalPolicy.DESTROY)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkWithChatgptQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+
