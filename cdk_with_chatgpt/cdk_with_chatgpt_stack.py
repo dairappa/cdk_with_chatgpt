531fbeb0d7fd87e144266101a6ea14d45700a473
dairappa <dairappa@gmail.com>
2023-04-06 22:41:39 +0000

    Add pytest for testing S3 constructs

--- cdk_with_chatgpt/cdk_with_chatgpt_stack.py
+++ cdk_with_chatgpt/cdk_with_chatgpt_stack.py
@@ -6,13 +6,16 @@ from aws_cdk.aws_s3 import Bucket, BlockPublicAccess
 
 class CdkWithChatgptStack(Stack):
 
+    destination_bucket: Bucket
+
+
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         my_account_id = core.Aws.ACCOUNT_ID
         bucket_name = f"destination-{my_account_id}"
 
-        bucket = Bucket(self, "DestinationBucket",
+        self.destination_bucket = Bucket(self, "DestinationBucket",
             bucket_name=bucket_name,
             block_public_access=BlockPublicAccess.BLOCK_ALL,
             removal_policy=RemovalPolicy.DESTROY,
--- tests/unit/test_cdk_with_chatgpt_stack.py
+++ tests/unit/test_cdk_with_chatgpt_stack.py
@@ -1,15 +1,68 @@
+import re
+import pytest
 import aws_cdk as core
-import aws_cdk.assertions as assertions
+from aws_cdk.aws_s3 import Bucket
+from constructs import Construct
 
 from cdk_with_chatgpt.cdk_with_chatgpt_stack import CdkWithChatgptStack
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in cdk_with_chatgpt/cdk_with_chatgpt_stack.py
-def test_sqs_queue_created():
-    app = core.App()
-    stack = CdkWithChatgptStack(app, "cdk-with-chatgpt")
-    template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+
+def get_stack() -> CdkWithChatgptStack:
+    app = core.App()
+    return CdkWithChatgptStack(app, "MyTestStack")
+
+
+def test_stack_has_bucket():
+    stack = get_stack()
+    assert stack.destination_bucket is not None, "Expected a Bucket in the stack"
+
+def test_bucket_name():
+    app = core.App()
+    stack = CdkWithChatgptStack(app, "MyTestStack")
+    template = app.synth().get_stack_by_name("MyTestStack").template
+
+    # Find the AWS::S3::Bucket resource in the template
+    bucket_resource = None
+    for _, resource in template['Resources'].items():
+        if resource['Type'] == 'AWS::S3::Bucket':
+            bucket_resource = resource
+            break
+
+    assert bucket_resource is not None, "Expected a Bucket resource in the template"
+
+    # Check the bucket name
+    bucket_name_fn_join = bucket_resource['Properties']['BucketName']
+    assert bucket_name_fn_join["Fn::Join"][1][0] == "destination-", \
+        f"Expected bucket name to start with 'destination-', but got '{bucket_name_fn_join}'"
+
+
+def test_bucket_properties():
+    app = core.App()
+    stack = CdkWithChatgptStack(app, "MyTestStack")
+    template = app.synth().get_stack_by_name("MyTestStack").template
+
+    # Find the AWS::S3::Bucket resource in the template
+    bucket_resource = None
+    for _, resource in template['Resources'].items():
+        if resource['Type'] == 'AWS::S3::Bucket':
+            bucket_resource = resource
+            break
+
+    assert bucket_resource is not None, "Expected a Bucket resource in the template"
+
+    # Check block public access settings
+    block_public_access = bucket_resource['Properties']['PublicAccessBlockConfiguration']
+    assert block_public_access == {
+        'BlockPublicAcls': True,
+        'BlockPublicPolicy': True,
+        'IgnorePublicAcls': True,
+        'RestrictPublicBuckets': True
+    }, "Expected public access to be blocked for the Bucket"
+
+    # Check removal policy
+    assert bucket_resource['DeletionPolicy'] == 'Delete', "Expected removal policy to be Delete"
+
+    # Check versioning
+    assert bucket_resource['Properties']['VersioningConfiguration']['Status'] == 'Enabled', \
+        "Expected Bucket to have versioning enabled"
